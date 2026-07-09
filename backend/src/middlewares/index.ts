4c5a1e42554c088f0fe4d52798ffded68b6f9858
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-09 15:22:52 +0100

    Delete backend directory

--- backend/src/middlewares/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-import { registerErrorHandler } from "./error";
-
-export async function registerMiddlewares(
-  app: FastifyInstance
-): Promise<void> {
-  await registerErrorHandler(app);
-}
