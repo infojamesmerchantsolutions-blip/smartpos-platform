0b84d1f278a93b508abfeb4199d54acdf2314855
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-09 16:28:32 +0100

    Update prisma.ts

--- backend/src/pluggins/prisma.ts
+++ backend/src/pluggins/prisma.ts
@@ -1,1 +1,21 @@
+import fp from 'fastify-plugin';
+import { FastifyInstance } from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
+}
+
+export const registerPrisma = fp(async (app: FastifyInstance) => {
+  await prisma.$connect();
+
+  app.decorate('prisma', prisma);
+
+  app.addHook('onClose', async () => {
+    await prisma.$disconnect();
+  });
+});
