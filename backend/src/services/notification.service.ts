4f0305b11c65f5ebe8c7dc3cd82349159ca83c2a
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-10 20:32:14 +0100

    Update notification.service.ts

--- backend/src/services/notification.service.ts
+++ backend/src/services/notification.service.ts
@@ -1,1 +1,89 @@
+import EmailService from "./email.service";
+import SmsService from "./sms.service";
+import PushNotificationService from "./push-notification.service";
 
+export default class NotificationService {
+
+  constructor(
+
+    private readonly email =
+
+      new EmailService(),
+
+    private readonly sms =
+
+      new SmsService(),
+
+    private readonly push =
+
+      new PushNotificationService()
+
+  ) {}
+
+  sendEmail(
+
+    to: string,
+
+    subject: string,
+
+    html: string
+
+  ) {
+
+    return this.email.send({
+
+      to,
+
+      subject,
+
+      html
+
+    });
+
+  }
+
+  sendSms(
+
+    to: string,
+
+    message: string
+
+  ) {
+
+    return this.sms.send({
+
+      to,
+
+      message
+
+    });
+
+  }
+
+  sendPush(
+
+    token: string,
+
+    title: string,
+
+    body: string,
+
+    data?: Record<string, any>
+
+  ) {
+
+    return this.push.send({
+
+      token,
+
+      title,
+
+      body,
+
+      data
+
+    });
+
+  }
+
+}
