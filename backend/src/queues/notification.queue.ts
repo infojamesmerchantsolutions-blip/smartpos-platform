62a5963fac3bb034d421edcbe6bbec524ea545aa
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-10 20:07:05 +0100

    Create notification.queue.ts

---
+++ backend/src/queues/notification.queue.ts
@@ -0,0 +1,99 @@
+import { Job, Worker } from "bullmq";
+
+import {
+
+  BullConnection
+
+} from "./bullmq.queue";
+
+export default function createNotificationWorker() {
+
+  return new Worker(
+
+    "notifications",
+
+    async (
+
+      job: Job
+
+    ) => {
+
+      const {
+
+        channel,
+
+        recipient,
+
+        subject,
+
+        message
+
+      } = job.data;
+
+      switch (
+
+        channel
+
+      ) {
+
+        case "email":
+
+          console.log(
+
+            "Email:",
+
+            recipient,
+
+            subject
+
+          );
+
+          break;
+
+        case "sms":
+
+          console.log(
+
+            "SMS:",
+
+            recipient
+
+          );
+
+          break;
+
+        case "push":
+
+          console.log(
+
+            "Push:",
+
+            recipient
+
+          );
+
+          break;
+
+        default:
+
+          console.log(
+
+            "Unknown notification channel."
+
+          );
+
+      }
+
+    },
+
+    {
+
+      connection:
+
+        BullConnection
+
+    }
+
+  );
+
+}
