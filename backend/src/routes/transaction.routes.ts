beb68f5089071861efc4fbc17e6cf7ff18cc81be
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-10 11:12:25 +0100

    Update transaction.routes.ts

--- backend/src/routes/transaction.routes.ts
+++ backend/src/routes/transaction.routes.ts
@@ -1,1 +1,46 @@
+import { FastifyInstance } from "fastify";
 
+import TransactionService from "../services/transaction.service";
+import TransactionController from "../controllers/transaction.controller";
+
+export default async function transactionRoutes(
+  app: FastifyInstance
+) {
+
+  const service =
+    new TransactionService(app);
+
+  const controller =
+    new TransactionController(service);
+
+  app.post(
+    "/transactions/start",
+    controller.start
+  );
+
+  app.post(
+    "/transactions/execute",
+    controller.execute
+  );
+
+  app.post(
+    "/transactions/settle",
+    controller.settle
+  );
+
+  app.post(
+    "/transactions/:id/complete",
+    controller.complete
+  );
+
+  app.post(
+    "/transactions/fail",
+    controller.fail
+  );
+
+  app.get(
+    "/transactions/:id",
+    controller.get
+  );
+
+}
