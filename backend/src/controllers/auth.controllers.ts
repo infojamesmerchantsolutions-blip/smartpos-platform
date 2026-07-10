6e58fa7b6c97363061fe224bd375621b4e4bc386
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-10 12:31:32 +0100

    Update auth.controllers.ts

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -1,1 +1,74 @@
+import {
+  FastifyReply,
+  FastifyRequest
+} from "fastify";
 
+import AuthService from "../services/auth.service";
+
+export default class AuthController {
+
+  constructor(
+
+    private readonly authService: AuthService
+
+  ) {}
+
+  login = async (
+
+    request: FastifyRequest,
+
+    reply: FastifyReply
+
+  ) => {
+
+    const body =
+      request.body as any;
+
+    const result =
+      await this.authService.login(
+
+        body.email,
+
+        body.password
+
+      );
+
+    return reply.send({
+
+      success: true,
+
+      data: result
+
+    });
+
+  };
+
+  me = async (
+
+    request: FastifyRequest,
+
+    reply: FastifyReply
+
+  ) => {
+
+    const user =
+      request.user as any;
+
+    const profile =
+      await this.authService.me(
+
+        user.id
+
+      );
+
+    return reply.send({
+
+      success: true,
+
+      data: profile
+
+    });
+
+  };
+
+}
